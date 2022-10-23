33e210043312f933b0657e42c4a277825f9374b6
fatihamarat <fatihhamarat@hotmail.com>
2022-10-23 23:21:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const getEmployees=document.getElementById("ajax");
+getEmployees.addEventListener("click",getAllemployees);
+function getAllemployees(){
+
+    const xhr=new XMLHttpRequest();
+
+    xhr.open("GET","employees.json",true);
+    
+    xhr.onload=function(){
+        let list=document.getElementById("employees");
+        if(this.status===200){
+            const employees=JSON.parse(this.responseText);
+            employees.forEach(function(employee){
+                list.innerHTML+=`
+                <tr>
+                    <td>${employee.name}</td>
+                    <td>${employee.department}</td>
+                    <td>${employee.salary}</td>
+                </tr>
+                `
+            })
+        }
+
+
+    }
+    
+    
+    xhr.send();
+}
